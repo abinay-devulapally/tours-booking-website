5901b12f5419c991770608b94510003746dae98a
abinay-devulapally <abinaykumar.d99@gmail.com>
2024-05-14 07:44:01 +0530

    Changed Mangoose Version

--- app.js
+++ app.js
@@ -19,6 +19,9 @@ const bookingRouter = require('./routers/bookingRoutes');
 
 const app = express();
 
+// Enable 'trust proxy' to parse X-Forwarded-For header
+app.set('trust proxy', true);
+
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
--- server.js
+++ server.js
@@ -16,16 +16,15 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD,
 );
 
+// const connectionOptions = {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// };
+
 mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log('DB connection successfull');
-  });
+  .connect(DB)
+  .then(() => console.log('DB connection successful'))
+  .catch((err) => console.error('DB connection error:', err));
 
 const port = process.env.PORT || 3000;
 
